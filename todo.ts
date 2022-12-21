d4f890c9f93796e66767951f96e774e474ffb72d
Shoaib Ahmed <115498101+me131112@users.noreply.github.com>
2022-12-21 15:48:28 +0500

    Add files via upload

---
+++ todo.ts
@@ -0,0 +1,73 @@
+import inquirer from "inquirer"
+import chalk from "chalk"
+console.log(chalk.greenBright("Welcome to Shoaib Todo List!"))
+interface Answers {
+    todo: string
+    deadline: string
+}
+const todos: any[] = [];
+const todosD: any[] = [];
+async function askQuestion(){
+    await inquirer
+    .prompt([
+        /* Pass your questions in here */
+        {
+            type: "string",
+            name: "todo",
+            message: chalk.blueBright("Enter your Todo task here!"),
+            validate: async (todo) => {
+                if (todo == ""){
+                    return chalk.blueBright("Please enter To-do task!")
+                }
+                return true
+            },
+        },
+        {
+            type: "string",
+            name: "deadline",
+            message: chalk.greenBright(`Enter the deadline for above task here!`),
+            validate: async (todo) => {
+                if (todo == ""){
+                    return chalk.greenBright("Please enter deadline for the task!")
+                }
+                return true
+            },
+            when(answers){
+                return answers.todo
+            },
+        }
+        ])
+    .then((answers: Answers) => {
+    if (answers.todo && answers.deadline){
+        todos.push(answers.todo);
+        todosD.push(answers.deadline)
+    } else{
+        console.log(chalk.blueBright("Write a to-do task!"));
+    }
+    if(todos.length > 0){
+        console.log(`Your Todo list contains: `)
+        for(let i=0;i<todos.length;i++){
+            console.log(chalk.blueBright(`Task: ${todos[i]}, Deadline: ${chalk.greenBright(todosD)}`))
+        }
+        // todos.forEach((x) => {console.log(x,"    -----------    ", )})
+    }
+    else if (todos.length == 0){
+        console.log(chalk.blueBright("Your Todo list is empty!"))
+    }
+    })
+}
+
+async function startagain(){
+    let again: any;
+    do{
+        await askQuestion();
+        again = await inquirer
+        .prompt({
+        type: "input",
+        name: "restart",
+        message: chalk.yellowBright("Do you want to continue adding more to-do tasks?"),
+        default: "(Y or N)"
+    })
+    }while(again.restart == 'y' || again.restart == 'Y');
+}
+startagain();
